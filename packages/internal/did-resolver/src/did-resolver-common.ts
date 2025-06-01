1db722aea2da40284894ff730207a6f7e077031d
Mike Kasprzak <mike@distraction.engineer>
2025-06-01 05:36:27 -0400

    more pieces to support did:apub

--- packages/internal/did-resolver/src/did-resolver-common.ts
+++ packages/internal/did-resolver/src/did-resolver-common.ts
@@ -1,20 +1,22 @@
 import { DidResolverBase } from './did-resolver-base.js'
+import { DidAPubMethod, DidAPubMethodOptions } from './methods/apub.js'
 import { DidPlcMethod, DidPlcMethodOptions } from './methods/plc.js'
 import { DidWebMethod, DidWebMethodOptions } from './methods/web.js'
 import { Simplify } from './util.js'
 
 export type DidResolverCommonOptions = Simplify<
-  DidPlcMethodOptions & DidWebMethodOptions
+  DidPlcMethodOptions & DidWebMethodOptions & DidAPubMethodOptions
 >
 
 export class DidResolverCommon
-  extends DidResolverBase<'plc' | 'web'>
-  implements DidResolverBase<'plc' | 'web'>
+  extends DidResolverBase<'plc' | 'web' | 'apub'>
+  implements DidResolverBase<'plc' | 'web' | 'apub'>
 {
   constructor(options?: DidResolverCommonOptions) {
     super({
       plc: new DidPlcMethod(options),
       web: new DidWebMethod(options),
+      apub: new DidAPubMethod(options),
     })
   }
 }
--- packages/internal/did-resolver/src/methods.ts
+++ packages/internal/did-resolver/src/methods.ts
@@ -1,2 +1,3 @@
 export * from './methods/plc.js'
 export * from './methods/web.js'
+export * from './methods/apub.js'
