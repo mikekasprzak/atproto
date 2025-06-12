7d0b72200430a02affa7c04c6e0bdaa41e439f34
Mike Kasprzak <mike@distraction.engineer>
2025-06-12 14:35:56 -0400

    committing so I can remove thnigs

--- lexicons/generate.sh
+++ lexicons/generate.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
 
-# recursively convert all jsonc files to json, and place them in ../dist/lexicons
-mkdir -p ../dist/lexicons
-for file in $(find . -type f -name "*.jsonc"); do
+output_dir="../dist/lexicons"
+
+# get a recursive list of all files
+files=$(find . -type f)
+
+# create the dist/lexicons output directory
+mkdir -p $output_dir
+
+# recursively convert all jsonc files with the .jsonld.jsonc or .jsonc extension to json, place them in $output_dir, then remove them from the list
+for file in $files; do
     echo "Converting $file to json"
-    pnpm exec jsonc-parser $file > ../dist/lexicons/${file%.jsonc}.json
+    if [[ "$file" == *.jsonld.jsonc ]]; then
+        # drop the .jsonc extension
+        pnpm exec jsonc-parser $file > $output_dir/${file%.jsonc}
+    elif [[ "$file" == *.jsonc ]]; then
+        # replace the .jsonc extension with .json
+        pnpm exec jsonc-parser $file > $output_dir/${file%.jsonc}.json
+    fi
+    files=$(echo "$files" | grep -v "$file")
 done
 
 # recursively convert all .jsonld.json files to json, and place them in ../dist/lexicons
