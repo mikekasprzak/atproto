cfa9a6797f196855b0bb23695a11b2ba6782a8f8
Mike Kasprzak <mike@distraction.engineer>
2025-06-19 01:36:22 -0400

    sort of fixes import bug

--- packages/pds-activitypub/src/index.ts
+++ packages/pds-activitypub/src/index.ts
@@ -1,4 +1,3 @@
-//export * from './util'
-//export * as oauthAuthorizationServer from './oauth/oauthAuthorizationServer'
 export * as wellKnown from './well-known'
-export * as apiOrgW3 from './api/org/w3'
+export { default as apiOrgW3 } from './api/org/w3'
+export { Server } from './lexicon'
--- packages/pds/src/api/index.ts
+++ packages/pds/src/api/index.ts
@@ -1,4 +1,4 @@
-import { apiOrgW3 } from '@atproto/pds-activitypub'
+import { Server as ActivityPubServer, apiOrgW3 } from '@atproto/pds-activitypub'
 import { AppContext } from '../context'
 import { Server } from '../lexicon'
 import appBsky from './app/bsky'
@@ -7,6 +7,6 @@ import comAtproto from './com/atproto'
 export default function (server: Server, ctx: AppContext) {
   comAtproto(server, ctx)
   appBsky(server, ctx)
-  apiOrgW3.default(server, ctx)
+  apiOrgW3(server as ActivityPubServer, ctx)
   return server
 }
